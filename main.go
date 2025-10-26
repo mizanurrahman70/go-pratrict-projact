e6cf49138378d88dcb3b60296acf84c0261f98d4
Md Mizanur Rahman <102843376+mizanurrahman70@users.noreply.github.com>
2025-10-26 22:12:56 +0600

    added function go land

--- main.go
+++ main.go
@@ -1,14 +1,16 @@
-// package main
 
-// import "fmt"
-
-// func main() {
-//     fmt.Println("Hello, World!")
-// }
 package main
 import "fmt"
+
+func add (number1 int,number2 int) {
+   sum := number1 + number2;
+   fmt.Println(sum);
+
+}
 func main (){
-	fmt.Println("hello , this is my fast go proajct")
-	a := 70;
-	fmt.Println(a)
+	// fmt.Println("hello , this is my fast go proajct")
+	// a := 70;
+	// fmt.Println(a)
+	add(50,60)
+
 }
